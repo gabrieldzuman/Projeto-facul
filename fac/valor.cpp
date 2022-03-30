12a9604e836a458fef90177e8f63fe9be59f47aa
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2022-03-30 00:13:24 -0300

    Add files via upload

---
+++ fac/valor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    float valor1, valor2, div, res;
+    cout<<"Digite um numero: "; //cout = ESCREVA
+    cin>>valor1; //cin = LEIA
+    cout<<"Digite outro numero: ";
+    cin>>valor2;
+    soma = valor1/valor2;
+    res = div;
+    cout<<"A resposta e: "<<res;
+  getch(); 
+}

32558e86e7b785f77ad442bfe1a3059828b259fa
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2023-10-06 01:22:34 -0300

    Update valor.cpp

--- fac/valor.cpp
+++ fac/valor.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 using namespace std;
-int main()
-{
-    float valor1, valor2, div, res;
-    cout<<"Digite um numero: "; //cout = ESCREVA
-    cin>>valor1; //cin = LEIA
-    cout<<"Digite outro numero: ";
-    cin>>valor2;
-    soma = valor1/valor2;
-    res = div;
-    cout<<"A resposta e: "<<res;
-  getch(); 
+
+int main() {
+    float valor1, valor2, soma, res; // Corrigi a declaração das variáveis
+    cout << "Digite um numero: "; // cout = ESCREVA
+    cin >> valor1; // cin = LEIA
+    cout << "Digite outro numero: ";
+    cin >> valor2;
+
+    soma = valor1 + valor2; // Corrigi para a soma em vez de divisão
+    res = soma; // Corrigi para atribuir a soma à variável 'res'
+
+    cout << "A resposta e: " << res << endl; // Adicionei endl para pular linha
+    return 0; // Adicionei a declaração de retorno
 }
